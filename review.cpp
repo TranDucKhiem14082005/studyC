9fa7cd88db9e057134d07b8c34ddb224fc1854d8
TranDucKhiem14082005 <khiemtran015@gmail.com>
2024-09-12 23:12:26 +0700

    review: C-Day1

---
+++ review.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	printf("nhap n:");
+	scanf("%d",&n);
+//	if(n%2==0) {
+//		printf("1");
+//	} else printf("0");
+//	if(n%3 == 0 && n % 5 == 0){
+//		printf("1");
+//	}
+//	else printf("0");
+//	if(n%3==0 && n%7!=0) {
+//		printf("1");
+//	}
+//	else {
+//		printf("0");
+//	}
+//	if(n%3==0 || n%7==0){
+//		printf("28tech va laptrinhC");
+//	} else printf("28tech.com.vn");
+//	if(n>30 && n < 50) {
+//		printf("1");
+//	}else printf("0");
+//	if(n>=30 && (n%2==0||n%3==0||n%5==0)){
+//		printf("1");
+//	}else printf("0");
+//	int r = n % 10;
+//	if((n>=10 && n<=99) &&(r==3||r==5||r==7)){
+//		printf("1");
+//	}
+//	else printf("0");
+//	if(n<=100 && n%23==0){
+//		printf("1");
+//	}
+//	else printf("0");
+	if(!(n>=10 && n<=20)){
+		printf("1");
+	}
+	else printf("0");
+}
